d412a5de325577644607fc953ab3e8ffd7b24a1d
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2026-03-02 05:09:48 +0100

    completed the task2

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,10 +1,13 @@
 package mate.academy
 
-object LetterRemover {
+class LetterRemover private constructor() {
+    companion object {
+
         @JvmStatic
         fun removeLetter(input: String): CharArray {
             return input.lowercase()
                 .replace('r', '!')
                 .toCharArray()
         }
+    }
 }
